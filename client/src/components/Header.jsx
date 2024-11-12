aa1a7a00fb7809a0f85903db01f29f849e4840d4
ashhwanigupta <Ashhwanigupta1@gmail.com>
2024-11-12 19:16:11 +0530

    create header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -33,7 +33,7 @@ const Header = () => {
           <FaMoon />
         </Button>
         <Link to="sign-in">
-          <Button className="" gradientDuoTone="purpleToBlue">
+          <Button className="" gradientDuoTone="purpleToBlue" outline>
             Sign In 
           </Button>
         </Link>
